ca1190343190932fb0582db359c3dac2befbe004
Bob Loblaw <razvanmandache@gmail.com>
2023-10-15 00:42:05 +0200

    Posts layout

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,18 +1,25 @@
 require('dotenv').config();
 let process = require('process');
+const { EleventyRenderPlugin } = require('@11ty/eleventy');
 const readDirRecursively = require('./utils/readDirRecursively');
+const fallbackAvatarUrl = require('./src/shortcodes/fallbackAvatarUrl');
+
 const mockDataDirectory = '_mockData/';
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setServerOptions({
-    watch: ['_site/**/*.css'],
+    watch: ['_site/**/*.css', '_mockData/**/*.json'],
   });
+
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
 
   eleventyConfig.addPassthroughCopy('src/assets');
 
   eleventyConfig.setLiquidOptions({
     jsTruthy: true,
   });
+
+  eleventyConfig.addShortcode('fallbackAvatarUrl', fallbackAvatarUrl);
 
   if (process.env.environment === 'Development') {
     eleventyConfig.addWatchTarget('./_mockData/');
